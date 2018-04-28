0e7beb380e64a96443094c6543a4eeacaeb3173e
Alex Schastny <alexey.schastniy@weavora.com>
2018-04-28 12:35:10 +0300

    change JiraApi auth method

--- src/GitHelper/Command/CreateCommand.php
+++ src/GitHelper/Command/CreateCommand.php
@@ -2,10 +2,9 @@
 
 namespace GitHelper\Command;
 
-use GitHelper\JiraApi\AdvancedApi;
-use GitHelper\JiraApi\AdvancedCurlClient;
-use GitHelper\JiraApi\AdvancedIssue;
-use GitHelper\JiraApi\HtAccessCookieAuth;
+use chobie\Jira\Api;
+use chobie\Jira\Api\Authentication\Basic;
+use chobie\Jira\Issue;
 use Symfony\Component\Console\Input\InputArgument;
 
 class CreateCommand extends BaseCommand
@@ -23,15 +22,43 @@ class CreateCommand extends BaseCommand
     {
         $params = $this->getParameters();
 
-        $api = new AdvancedApi($params['url'],
-            new HtAccessCookieAuth($params['login'], $params['password'], $params['htaccess_user'], $params['htaccess_pass']),
-            new AdvancedCurlClient()
+        $api = new Api($params['url'],
+            new Basic($params['login'], $params['password'])
         );
 
-        /** @var AdvancedIssue $issue */
-        $issue = $api->getIssue($params['project_key'] . '-' . $this->getArgument('number'));
-        $branch = $issue->getBranchNameByPattern($params['branch_template'], $params['branch_length_limit']);
+        $response = $api->getIssue($params['project_key'] . '-' . $this->getArgument('number'));
+        $branch = $this->getBranchNameByPattern($response->getResult(), $params['branch_template'], $params['branch_length_limit']);
 
         $this->getGit()->checkoutNewBranch($branch);
+    }
+
+    public function getBranchNameByPattern($result, $pattern, $limit)
+    {
+        $issue = new Issue($result);
+
+        $type = $issue->getFields()['issuetype']['name'];
+        $name = $issue->getFields()['summary'];
+        list($issueKey, $number) = explode('-', $issue->getKey());
+
+        $name = str_replace(' ', '-', strtolower($name));
+        $name = preg_replace('/[^A-Za-z0-9\-]/', '', $name);
+        $name = preg_replace('/-+/', '-', $name);
+
+        $name = strtr($pattern, [
+            '%type%' => strtolower($type) === 'bug' ? 'bug': 'feature',
+            '%name%' => $name,
+            '%number%' => $number,
+            '%key%' => $issueKey,
+        ]);
+
+        if (strpos($name, '%') !== false) {
+            throw new \Exception('Looks like using unavailable variable in pattern');
+        }
+
+        if (strlen($name) > $limit) {
+            $name = substr($name, 0, strrpos($name, '-', -1 * (strlen($name) - $limit)));
+        }
+
+        return $name;
     }
 }
